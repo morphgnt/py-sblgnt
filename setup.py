98d4348deffb663aeac0df1c38e53fd2cf058f27
James Tauber <jtauber@jtauber.com>
2014-07-23 21:53:37 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="py-sblgnt",
+    version="0.1",
+    description="pip-installable MorphGNT/SBLGNT with Python API",
+    license="MIT",
+    url="http://https://github.com/morphgnt/py-sblgnt",
+    author="James Tauber",
+    author_email="jtauber@jtauber.com",
+    packages=["pysblgnt"],
+    package_data={"": ["sblgnt/*.txt"]},
+)
