b9021e52934210b6b1a4a1bde5a6790ce796be2e
James Tauber <jtauber@jtauber.com>
2016-01-16 13:49:48 -0500

    bumped up version to 0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="py-sblgnt",
-    version="0.4",
+    version="0.5",
     description="pip-installable MorphGNT/SBLGNT with Python API",
     license="MIT",
     url="http://https://github.com/morphgnt/py-sblgnt",
