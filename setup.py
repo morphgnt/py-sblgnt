904fed08a14bffea6e1776d8c4c3eeb7c6092688
James Tauber <jtauber@jtauber.com>
2017-03-19 20:51:01 -0400

    bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="py-sblgnt",
-    version="0.9",
+    version="0.10",
     description="pip-installable MorphGNT/SBLGNT with Python API",
     license="MIT",
     url="https://github.com/morphgnt/py-sblgnt",
